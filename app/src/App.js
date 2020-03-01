6edeb730fbe0e16b26b3f43ef03358704412475b
Joseph Lin <joslin@microsoft.com>
2020-03-01 15:25:03 -0800

    Initial site

---
+++ app/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to our Static App site!
+        </p>
+        <a
+          className="App-link"
+          href="/.api/GetList"
+        >
+          Let's try some functions!
+        </a>
+        <a 
+          className="App-link"
+          href="/authenticated/"
+        >
+        You can only get here if you've logged in!
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
